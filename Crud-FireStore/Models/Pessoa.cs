61a7ee86b8604bee3d433cd70073251dadc47b66
lucas <lucas@DESKTOP-3P88PCA>
2021-08-09 00:04:48 -0300

    Add project files.

---
+++ Crud-FireStore/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_FireStore.Models
+{
+    [FirestoreData]
+    public class Pessoa
+    {
+        public string PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+    }
+}
